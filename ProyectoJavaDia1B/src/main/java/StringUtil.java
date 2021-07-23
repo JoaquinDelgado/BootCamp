3e88e48a38a02842dcc53be51c07b47a4f69e38b
Joaquin Delgado <joaquin.delgado@mercadolibre.com>
2021-07-23 17:31:08 -0300

    tarea 1 b

---
+++ ProyectoJavaDia1B/src/main/java/StringUtil.java
@@ -0,0 +1,45 @@
+public class StringUtil {
+        // Retorna una cadena compuesta por n caracteres c
+        public static String replicate(char c,int n)
+        {
+            //Implementación
+        }
+
+        // Retorna una cadena de longitud n, compuesta por s
+        // y precedida de tantos caracteres c como sea necesario
+        // para completar la longitud mencionada
+        public static String lpad(String s,int n,char c)
+        {
+            //Implementación
+        }
+
+        // Retorna un String[] conteniendo los elementos de arr
+        // representados como cadenas de caracteres
+        public static String[] toStringArray(int arr[])
+        {
+            //Implementación
+        }
+
+        // Retorna un String[] conteniendo los elementos de arr
+        // representados como cadenas de caracteres
+        public static int[] toIntArray(String arr[])
+        {
+            //Implementación
+        }
+
+
+        // Retorna la longitud del elemento con mayor cantidad
+        // de caracteres del array arr
+        public static int maxLength(String arr[])
+        {
+            //Implementación
+        }
+
+        // Completa los elemento del arr agregando caracteres c
+        // a la izquierda, dejando a todos con la longitud del mayor
+        public static void lNormalize(String arr[],char c)
+        {
+            //Implementación
+        }
+}
+
